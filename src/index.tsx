8250ea07ea1b71e58c9c6c55a25908eb56f5b8b0
willybrauner <willybrownhair@gmail.com>
2020-06-30 18:43:48 +0200

    Init new router'

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import "./index.less";
 import ReactDOM from "react-dom";
 import * as React from "react";
+import App from "./components/app/App";
+import Router, { IRoute } from "./Router";
 
-import App from "components/app/App";
 const debug = require("debug")(`front:index`);
 
 /**
@@ -10,9 +11,38 @@ const debug = require("debug")(`front:index`);
  */
 export function initApp() {
   /**
+   * Router
+   */
+
+  const prepareRoutes: IRoute[] = [
+    {
+      path: "/",
+      component: HomePage,
+    },
+    {
+      path: "/about",
+      component: AboutPage,
+    },
+  ];
+
+  const router = new Router({ base: "/", routes: prepareRoutes });
+
+  /**
    * Start
    */
   ReactDOM.render(<App />, document.getElementById("Root"));
 }
 
 initApp();
+
+/**
+ * Pages
+ * Important que ce soit des fonctions nommées
+ */
+export function HomePage() {
+  return <div className={"Home"}>Home</div>;
+}
+
+export function AboutPage() {
+  return <div className={"About"}>About</div>;
+}
