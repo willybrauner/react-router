8beca5d888395e3d9a496b73c441c848877c464a
willybrauner <mail@willybrauner.com>
2021-01-02 12:42:32 +0100

    remove

--- src/useRouter.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { RouterContext } from "./Router";
-
-/**
- * Return current router instance
- * Instance depend of inside of witch provider useRouter is called
- */
-export const useRouter = () => useContext(RouterContext);
